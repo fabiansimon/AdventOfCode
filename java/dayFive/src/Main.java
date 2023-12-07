a5b5f019b6eed8fa5020df960959a5b2ef86be55
Fabian Simon <77444050+fabiansimon@users.noreply.github.com>
2023-12-07 22:17:24 +0100

    ListNode

--- java/dayFive/src/Main.java
+++ java/dayFive/src/Main.java
@@ -22,10 +22,49 @@ public class Main {
                 continue;
             }
 
+            if (line.length() == 0) {
+                br.readLine();
+                line = br.readLine();
+                List<MapValue> input = new ArrayList<>();
 
+                while (line.length() > 0) {
+                    input.add(new MapValue(line));
+                    line = br.readLine();
+
+                    if (line == null) break;
+                }
+
+                ListMapNode node = new ListMapNode(input, new ListMapNode());
+            }
             line = br.readLine();
+
         }
 
         System.out.println(seeds);
+    }
+}
+
+class ListMapNode {
+    List<MapValue> mapValues;
+    ListMapNode next;
+
+    public ListMapNode() {}
+
+    public ListMapNode(List<MapValue> mapValues, ListMapNode next) {
+        this.mapValues = mapValues;
+        this.next = next;
+    }
+}
+
+class MapValue {
+    int destRange;
+    int srcRange;
+    int rangeLength;
+
+    public MapValue(String s) {
+        String[] data = s.split( " ");
+        this.destRange = Integer.parseInt(data[0]);
+        this.srcRange = Integer.parseInt(data[1]);;
+        this.rangeLength = Integer.parseInt(data[2]);
     }
 }
