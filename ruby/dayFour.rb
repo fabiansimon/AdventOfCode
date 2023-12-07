604458543117d41dc0fdb0315a51aac432edd1c2
Fabian Simon <fabian.simon98@gmail.com>
2023-12-07 15:52:50 +0100

    dayFour done (finally

--- ruby/dayFour.rb
+++ ruby/dayFour.rb
@@ -1,6 +1,9 @@
 require 'set'
 file_path = "./dayFour_input.txt"
 
+=begin
+PART I
+
 File.open(file_path, 'r') do |file|
     points = 0
 
@@ -24,3 +27,66 @@ File.open(file_path, 'r') do |file|
 
     puts points
 end
+=end
+
+# PART II
+
+def get_raw_data(path)
+    data = []
+    File.open(path, 'r') do |file|
+        
+        file.each_line do |line|
+            sides = line.split(":")[1].split("|")
+            curr = [sides[0].split, sides[1].split]
+            data.push(curr)
+        end
+
+    end
+
+    return data
+end
+
+def get_matches(data)
+    matches = []
+
+    data.each do |d| 
+        winning = Set.new(d[0])    
+        curr = 0
+
+        d[1].each do |nr| 
+            if winning.include?(nr)
+                curr += 1
+            end
+        end
+
+        matches.push(curr)
+    end
+
+    return matches
+
+end
+
+def get_total_scratchcards(matches)
+    length = matches.size
+    stratchcards = Array.new(length, 1)
+
+    (0..length-1).each do |i|
+        loops = matches[i]
+
+        loops.times do |j|
+            stratchcards[j+i+1] += 1*stratchcards[i]
+        end
+
+    end
+
+    return stratchcards.reduce(:+)
+end
+
+
+data = get_raw_data(file_path)
+
+matches = get_matches(data)
+
+res = get_total_scratchcards(matches)
+
+p res
