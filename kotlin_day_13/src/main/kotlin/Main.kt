ba14a947e02107524feef8278d06fa615fb53232
Fabian Simon <fabian.simon98@gmail.com>
2023-12-14 10:00:28 +0100

    day 13 Part I done

--- kotlin_day_13/src/main/kotlin/Main.kt
+++ kotlin_day_13/src/main/kotlin/Main.kt
@@ -6,10 +6,36 @@ fun main(args: Array<String>) {
     val path = "./day13_input.txt";
     var lines: List<List<String>> = getInputData(path);
 
-    println(findMirrorIndex(lines[0]))
+    var cols = 0;
+    var rows = 0;
+
+    for (line in lines) {
+        var index = findVerticalMirrorIndex(line);
+        if (index == -1) {
+            index = findHorizontalMirrorIndex(line)
+            rows += index
+        } else {
+            cols += index
+        }
+    }
+
+    println(cols + (rows * 100))
+
 }
 
-fun findMirrorIndex(lines: List<String>): Int {
+fun findHorizontalMirrorIndex(lines: List<String>): Int {
+    for (i in lines.indices) {
+        for (j in lines.indices.drop(i+1).reversed()) {
+            if (lines[i] == lines[j] && isSymmetric(lines, i, j)) {
+                return ((j+i)/2)+1
+            }
+            }
+        }
+
+    return -1
+}
+
+fun findVerticalMirrorIndex(lines: List<String>): Int {
     var firstLine = lines[0]
     var length = firstLine.length
 
@@ -17,7 +43,6 @@ fun findMirrorIndex(lines: List<String>): Int {
         if (i == length-1) break
 
         if (isSymmetric(firstLine.substring(i, length))) {
-
             var correct = true
             for (line in lines.drop(1)) {
                 if (!isSymmetric(line.substring(i, length))) {
@@ -26,16 +51,18 @@ fun findMirrorIndex(lines: List<String>): Int {
                 }
             }
 
-            if (correct) return ((length-i)/2)+1
+            if (correct) {
+                return (length + i)/2
+            }
         }
     }
 
     for (i in firstLine.indices.reversed()) {
-        if (i == 1) break
+        if (i == 0) break
 
         if (isSymmetric(firstLine.substring(0, i+1))) {
             var correct = true
-            for (line in lines.drop(1)) {
+            for (line in lines.drop(0)) {
                 if (!isSymmetric(line.substring(0, i+1))) {
                     correct = false
                     break
@@ -48,6 +75,23 @@ fun findMirrorIndex(lines: List<String>): Int {
 
     return -1
 }
+
+fun isSymmetric(lines: List<String>, bottom: Int, top: Int): Boolean {
+    var b = bottom
+    var t = top
+    while (b < t) {
+        if (lines[b++] != lines[t--]) return false
+    }
+
+    b = bottom
+    t = top
+    while (b >= 0 && t < lines.size) {
+        if (lines[b--] != lines[t++]) return false
+    }
+
+    return true;
+}
+
 fun isSymmetric(str: String): Boolean {
     var left = 0
     var right = str.length-1
