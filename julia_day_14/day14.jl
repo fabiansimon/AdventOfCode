6cac23f2a9cb2d9e56a640c207bb06ebb2a89518
Fabian Simon <fabian.simon98@gmail.com>
2023-12-16 17:03:17 +0100

    day 14 part I done

--- julia_day_14/day14.jl
+++ julia_day_14/day14.jl
@@ -16,31 +16,30 @@ function get_lines(path)
 end
 
 function get_total_weight(lines)
-    index = length(lines[1])
-    curr = 0
+    border = length(lines[1])
+    index = border
+    total = 0
     for line in lines
-        index = length(line)
+        index = border
         for (i, c) in enumerate(line)
-            if c == '#' 
-                index = i-1
+            if (c == 'O')
+                total += index
+                index -= 1
             end
 
-            if c == 'O'
-                curr += index + 1
-                index -= 1
+            if (c == '#') 
+                index = border-i
             end
         end
     end
 
-    return curr
+    return total
 end
+
 
 path = "./day14_input.txt"
 lines = get_lines(path)
 total_weight = get_total_weight(lines)
 
-
-
-print(total_weight)
-
+println(total_weight)
 
