dfe1ece6ba42441a525d9e310b942213dc524bda
Fabian Simon <fabian.simon98@gmail.com>
2023-12-18 15:05:14 +0100

    day15 finished

--- f#_day_15/day15/day15/Program.fs
+++ f#_day_15/day15/day15/Program.fs
@@ -1,4 +1,8 @@
 ﻿open System.IO
+open System.Collections.Generic
+
+let multiplier = 17
+let size = 256
 
 let readTextFile (path: string) =
     try
@@ -9,26 +13,98 @@ let readTextFile (path: string) =
         printfn "File not found: %s" ex.Message
         ""
 
+let getHash (str: string): int =
+    let mutable hash = 0
+
+    for c in str do
+        if (int c) <> 10 then
+            hash <- hash + (int c)
+            hash <- hash * multiplier
+            hash <- hash % size
+
+    hash
+
 let hashSequence (data: string[]): int =
-    let mutable value = 0
     let mutable total = 0
-    let multiplier = 17
-    let max = 256
     for sequence in data do
-        value <- 0
-        for c in sequence do
-            if (int c) <> 10 then
-                value <- value + (int c)
-                value <- value * multiplier
-                value <- value % max
-                
-        total <- total + value
+        total <- total + getHash sequence
+
     total
 
+let rawHash (str: string): string =
+    if str.Contains("=") then
+        str.Split('=').[0]
+    else
+        str.Split('-').[0]
+
+let checkKey (lense: string, key: string): bool =
+    let mutable index = 0
+    let length = min (String.length lense) (String.length key)
+
+    while index < length && lense.[index] = key.[index] do
+        index <- index + 1
+
+    index = length
+
+
+
+// ==== Functionality ==== 
+    
 let content = readTextFile "../../../../day15_input.txt"
 let data = content.Split([|','|])
 
-let value = hashSequence data
+let test = "ot"
+let value = getHash test
 
-printfn "%d" value
-// printfn "%d" value
+let boxes : List<string>[] = Array.init size (fun _ -> List<string>())
+let values = new Dictionary<string, int>()
+
+for d in data do
+    let rawHash = rawHash d
+    let index = getHash rawHash
+
+    let mutable i = 0
+
+    if d.Contains("-") then // Delete
+
+        let key = d.Split("-").[0]
+
+        while i < boxes.[index].Count do
+            let lense = boxes.[index].[i]
+
+            if lense = key then
+                boxes.[index].RemoveAt(i)
+                values.Remove(key)
+                i <- boxes.[index].Count + 10
+            else
+                i <- i + 1
+
+    else
+
+        let key = d.Split("=").[0]
+        let value = d.Split("=").[1]
+
+        while i < boxes.[index].Count do
+            let lense = boxes.[index].[i]
+
+            if lense = key then
+                values.[lense] <- (int value)
+                i <- boxes.[index].Count + 10
+            else
+                i <- i + 1
+
+        if i <> boxes.[index].Count + 10 then
+            boxes.[index].Add(key)
+            values.Add(key, (int value))
+
+let mutable power = 0
+
+for i = 0 to size - 1 do
+    for j = 0 to boxes.[i].Count - 1 do
+        let lense = boxes.[i].[j]
+        let focal = values.[lense]
+
+        power <- power + ((i+1) * (j+1) * focal)
+
+
+printfn "%d" power
