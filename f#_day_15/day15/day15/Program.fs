b87afd4ef41168063d1946c57a3f8f8a50c335b7
Fabian Simon <fabian.simon98@gmail.com>
2023-12-18 10:40:30 +0100

    day15  PART I

---
+++ f#_day_15/day15/day15/Program.fs
@@ -0,0 +1,34 @@
+﻿open System.IO
+
+let readTextFile (path: string) =
+    try
+        use sr = new StreamReader(path)
+        sr.ReadToEnd()
+    with
+    | :? System.IO.FileNotFoundException as ex ->
+        printfn "File not found: %s" ex.Message
+        ""
+
+let hashSequence (data: string[]): int =
+    let mutable value = 0
+    let mutable total = 0
+    let multiplier = 17
+    let max = 256
+    for sequence in data do
+        value <- 0
+        for c in sequence do
+            if (int c) <> 10 then
+                value <- value + (int c)
+                value <- value * multiplier
+                value <- value % max
+                
+        total <- total + value
+    total
+
+let content = readTextFile "../../../../day15_input.txt"
+let data = content.Split([|','|])
+
+let value = hashSequence data
+
+printfn "%d" value
+// printfn "%d" value
